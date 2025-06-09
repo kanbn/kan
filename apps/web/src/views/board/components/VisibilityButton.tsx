c7f4e5917268ae1d79f0fb2b26ef255f785cd986
Henry <henry_ball@hotmail.co.uk>
2025-06-09 21:27:26 +0100

    feat: tweak design

--- apps/web/src/views/board/components/VisibilityButton.tsx
+++ apps/web/src/views/board/components/VisibilityButton.tsx
@@ -1,15 +1,9 @@
-import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-import {
-  HiOutlineEye,
-  HiOutlineEyeSlash,
-  HiOutlineLink,
-} from "react-icons/hi2";
+import { HiOutlineEye, HiOutlineEyeSlash } from "react-icons/hi2";
 
 import Button from "~/components/Button";
 import CheckboxDropdown from "~/components/CheckboxDropdown";
 import { usePopup } from "~/providers/popup";
-import { useWorkspace } from "~/providers/workspace";
 import { api } from "~/utils/api";
 
 interface QueryParams {
@@ -21,7 +15,6 @@ interface QueryParams {
 const VisibilityButton = ({
   visibility,
   boardPublicId,
-  boardSlug,
   queryParams,
   isLoading,
   isAdmin,
@@ -34,12 +27,10 @@ const VisibilityButton = ({
   isAdmin: boolean;
 }) => {
   const { showPopup } = usePopup();
-  const { workspace } = useWorkspace();
   const utils = api.useUtils();
   const [stateVisibility, setStateVisibility] = useState<"public" | "private">(
     visibility,
   );
-  const router = useRouter();
 
   useEffect(() => {
     setStateVisibility(visibility);
@@ -49,7 +40,6 @@ const VisibilityButton = ({
 
   const updateBoardVisibility = api.board.update.useMutation({
     onSuccess: () => {
-      setStateVisibility(isPublic ? "private" : "public");
       showPopup({
         header: "Board visibility updated",
         message: `The visibility of your board has been set to ${isPublic ? "public" : "private"}.`,
@@ -83,37 +73,22 @@ const VisibilityButton = ({
             selected: !isPublic,
           },
         ]}
-        handleSelect={(_g, i) =>
+        handleSelect={(_g, i) => {
+          setStateVisibility(isPublic ? "private" : "public");
           updateBoardVisibility.mutate({
             visibility: i.key as "public" | "private",
             boardPublicId,
-          })
-        }
+          });
+        }}
+        menuSpacing="md"
       >
         <Button
           variant="secondary"
           iconLeft={isPublic ? <HiOutlineEye /> : <HiOutlineEyeSlash />}
-          isLoading={updateBoardVisibility.isPending}
           disabled={isLoading || !isAdmin}
         >
-          {isPublic ? "Public" : "Private"}
+          Visibility
         </Button>
-        {isPublic && (
-          <button
-            type="button"
-            onClick={(e) => {
-              e.preventDefault();
-              e.stopPropagation();
-              router.push("/" + workspace.slug + "/" + boardSlug);
-            }}
-            aria-label="Clear filters"
-            className="absolute -right-[8px] -top-[8px] flex h-5 w-5 items-center justify-center rounded-full border-2 border-light-100 bg-light-1000 text-[8px] font-[700] text-light-600 dark:border-dark-50 dark:bg-dark-1000 dark:text-dark-600"
-          >
-            <span className="text-light-50 dark:text-dark-50">
-              <HiOutlineLink size={12} />
-            </span>
-          </button>
-        )}
       </CheckboxDropdown>
     </div>
   );
