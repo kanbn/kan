3fdd8d3cd2997e319ab146400b3dcf28dd301f50
LovelessCodes <hello@loveless.codes>
2025-06-09 10:18:17 +0000

    feat: enhance board visibility controls with dropdown menu and success notifications

--- apps/web/src/views/board/components/VisibilityButton.tsx
+++ apps/web/src/views/board/components/VisibilityButton.tsx
@@ -1,8 +1,15 @@
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-import { HiOutlineEye, HiOutlineEyeSlash } from "react-icons/hi2";
+import {
+  HiOutlineEye,
+  HiOutlineEyeSlash,
+  HiOutlineLink,
+} from "react-icons/hi2";
 
 import Button from "~/components/Button";
+import CheckboxDropdown from "~/components/CheckboxDropdown";
 import { usePopup } from "~/providers/popup";
+import { useWorkspace } from "~/providers/workspace";
 import { api } from "~/utils/api";
 
 interface QueryParams {
@@ -14,21 +21,25 @@ interface QueryParams {
 const VisibilityButton = ({
   visibility,
   boardPublicId,
+  boardSlug,
   queryParams,
   isLoading,
   isAdmin,
 }: {
   visibility: "public" | "private";
   boardPublicId: string;
+  boardSlug: string;
   queryParams: QueryParams;
   isLoading: boolean;
   isAdmin: boolean;
 }) => {
   const { showPopup } = usePopup();
+  const { workspace } = useWorkspace();
   const utils = api.useUtils();
   const [stateVisibility, setStateVisibility] = useState<"public" | "private">(
     visibility,
   );
+  const router = useRouter();
 
   useEffect(() => {
     setStateVisibility(visibility);
@@ -39,6 +50,11 @@ const VisibilityButton = ({
   const updateBoardVisibility = api.board.update.useMutation({
     onSuccess: () => {
       setStateVisibility(isPublic ? "private" : "public");
+      showPopup({
+        header: "Board visibility updated",
+        message: `The visibility of your board has been set to ${isPublic ? "public" : "private"}.`,
+        icon: "success",
+      });
     },
     onError: () => {
       showPopup({
@@ -52,23 +68,54 @@ const VisibilityButton = ({
     },
   });
 
-  const handleUpdateBoardVisibility = () => {
-    updateBoardVisibility.mutate({
-      visibility: isPublic ? "private" : "public",
-      boardPublicId,
-    });
-  };
-
   return (
-    <Button
-      variant="secondary"
-      onClick={handleUpdateBoardVisibility}
-      iconLeft={isPublic ? <HiOutlineEye /> : <HiOutlineEyeSlash />}
-      isLoading={updateBoardVisibility.isPending}
-      disabled={isLoading || !isAdmin}
-    >
-      {isPublic ? "Public" : "Private"}
-    </Button>
+    <div className="relative">
+      <CheckboxDropdown
+        items={[
+          {
+            key: "public",
+            value: "Public",
+            selected: isPublic,
+          },
+          {
+            key: "private",
+            value: "Private",
+            selected: !isPublic,
+          },
+        ]}
+        handleSelect={(_g, i) =>
+          updateBoardVisibility.mutate({
+            visibility: i.key as "public" | "private",
+            boardPublicId,
+          })
+        }
+      >
+        <Button
+          variant="secondary"
+          iconLeft={isPublic ? <HiOutlineEye /> : <HiOutlineEyeSlash />}
+          isLoading={updateBoardVisibility.isPending}
+          disabled={isLoading || !isAdmin}
+        >
+          {isPublic ? "Public" : "Private"}
+        </Button>
+        {isPublic && (
+          <button
+            type="button"
+            onClick={(e) => {
+              e.preventDefault();
+              e.stopPropagation();
+              router.push("/" + workspace.slug + "/" + boardSlug);
+            }}
+            aria-label="Clear filters"
+            className="absolute -right-[8px] -top-[8px] flex h-5 w-5 items-center justify-center rounded-full border-2 border-light-100 bg-light-1000 text-[8px] font-[700] text-light-600 dark:border-dark-50 dark:bg-dark-1000 dark:text-dark-600"
+          >
+            <span className="text-light-50 dark:text-dark-50">
+              <HiOutlineLink size={12} />
+            </span>
+          </button>
+        )}
+      </CheckboxDropdown>
+    </div>
   );
 };
 
