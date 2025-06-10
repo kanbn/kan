5d2101b7bcac54f29ee2cca7830073ceddbd5015
LovelessCodes <hello@loveless.codes>
2025-06-10 08:33:46 +0000

    refactor: generalize integration authorization flow and improve error handling

--- packages/api/src/routers/integration.ts
+++ packages/api/src/routers/integration.ts
@@ -10,6 +10,10 @@ const urls = {
   trello: "https://api.trello.com/1/authorize"
 }
 
+const apiKeys = {
+  trello: process.env.TRELLO_APP_API_KEY,
+}
+
 export const integrationRouter = createTRPCRouter({
   providers: protectedProcedure.query(async ({ ctx }) => {
     const user = ctx.user;
@@ -68,48 +72,47 @@ export const integrationRouter = createTRPCRouter({
   getAuthorizationUrl: protectedProcedure
       .meta({
         openapi: {
-          summary: "Get authorization URL for Trello",
+          summary: "Get authorization URL for an integration",
           method: "GET",
-          path: "/trello/authorize",
-          description: "Retrieves the authorization URL for Trello",
-          tags: ["Trello"],
+          path: "/integration/authorize",
+          description: "Retrieves the authorization URL for an integration",
+          tags: ["Integration"],
           protect: true,
         },
       })
       .input(z.object({ provider: z.enum(["trello"]) }))
       .output(z.object({ url: z.string() }))
       .query(async ({ ctx, input }) => {
+        const apiKey = apiKeys[input.provider];
+
+        if (!apiKey)
+          throw new TRPCError({
+            message: `${input.provider.at(0)?.toUpperCase() + input.provider.slice(1)} API key not set in environment variables`,
+            code: "INTERNAL_SERVER_ERROR",
+          });
+  
+        const user = ctx.user;
+  
+        if (!user)
+          throw new TRPCError({
+            message: "User not authenticated",
+            code: "UNAUTHORIZED",
+          });
+  
+        const integration = await integrationsRepo.getProviderForUser(
+          ctx.db,
+          user.id,
+          input.provider,
+        );
+  
+        if (integration)
+          throw new TRPCError({
+            message: `${input.provider.at(0)?.toUpperCase() + input.provider.slice(1)} integration already exists`,
+            code: "BAD_REQUEST",
+          });
+
         if (input.provider === "trello") {
-          const apiKey = process.env.TRELLO_APP_API_KEY;
-    
-          if (!apiKey)
-            throw new TRPCError({
-              message: "Trello API key not set in environment variables",
-              code: "INTERNAL_SERVER_ERROR",
-            });
-    
-          const user = ctx.user;
-    
-          if (!user)
-            throw new TRPCError({
-              message: "User not authenticated",
-              code: "UNAUTHORIZED",
-            });
-    
-          const integration = await integrationsRepo.getProviderForUser(
-            ctx.db,
-            user.id,
-            input.provider,
-          );
-    
-          if (integration)
-            throw new TRPCError({
-              message: "Trello integration already exists",
-              code: "BAD_REQUEST",
-            });
-    
           const url = `${urls[input.provider]}?key=${apiKey}&expiration=never&response_type=token&scope=read&return_url=${env("NEXT_PUBLIC_BASE_URL")}/settings/trello/authorize&callback_method=fragment`;
-    
           return { url };
         }
 
