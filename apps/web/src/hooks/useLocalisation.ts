75558dcfc679a99e9fea661ea2e488546a5bdac5
Jovanio <jovaniojr3@gmail.com>
2025-12-09 11:08:45 -0300

    adjust pt-BR date format in date-fns

--- apps/web/src/hooks/useLocalisation.ts
+++ apps/web/src/hooks/useLocalisation.ts
@@ -1,6 +1,6 @@
 import type { Locale as DateFnsLocale } from "date-fns";
 import { useLingui } from "@lingui/react";
-import { de, enGB, es, fr, it, nl, pl, ru } from "date-fns/locale";
+import { de, enGB, es, fr, it, nl, pl, ru, ptBR} from "date-fns/locale";
 
 import type { Locale } from "~/locales";
 import { useLinguiContext } from "~/providers/lingui";
@@ -19,6 +19,7 @@ export function useLocalisation() {
     nl,
     ru,
     pl,
+    ptbr: ptBR,
   };
 
   const currentDateLocale = dateLocaleMap[locale] ?? enGB;
