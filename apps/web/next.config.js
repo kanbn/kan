bcd20b58c60928a856f252064a12e7299c702642
Henry <30578846+hjball@users.noreply.github.com>
2025-12-10 22:16:38 +0000

    chore: remove instrumentation (#279)
    * chore: remove instrumentation
    
    * chore: clean apk cache after installation
    
    * chore: fix docker builds

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -26,7 +26,6 @@ const config = {
   ],
 
   /** We already do linting and typechecking as separate tasks in CI */
-  eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
 
   images: {
