3f75828e6a65f2bf62c76d63e9a461e4d6a57256
Henry <henry_ball@hotmail.co.uk>
2026-03-01 21:56:14 +0000

    chore: remove duplicate col

--- packages/db/migrations/20260224105235_AddGitHubIntegrationSupport.sql
+++ packages/db/migrations/20260224105235_AddGitHubIntegrationSupport.sql
@@ -1,3 +1,2 @@
 ALTER TYPE "public"."source" ADD VALUE 'github';--> statement-breakpoint
 ALTER TABLE "integration" ALTER COLUMN "accessToken" SET DATA TYPE text;--> statement-breakpoint
-ALTER TABLE "workspace" ADD COLUMN "showEmailsToMembers" boolean DEFAULT true NOT NULL;
