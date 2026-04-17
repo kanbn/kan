4f4f507b582aebff6e231672ee8abdb4acabe36e
Henry <henry_ball@hotmail.co.uk>
2026-04-17 22:19:41 +0100

    fix: toggle spacing

--- apps/web/src/components/NewWorkspaceForm.tsx
+++ apps/web/src/components/NewWorkspaceForm.tsx
@@ -325,7 +325,7 @@ export function NewWorkspaceForm() {
       </div>
       <div
         className={twMerge(
-          "mt-6 flex items-center justify-end border-t border-light-600 px-5 pb-5 pt-5 dark:border-dark-600",
+          "mt-6 flex items-center justify-end space-x-4 border-t border-light-600 px-5 pb-5 pt-5 dark:border-dark-600",
           !showProBenefits && "mt-12",
         )}
       >
